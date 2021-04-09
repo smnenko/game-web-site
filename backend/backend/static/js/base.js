ee6f2d3b6b57384fdebee21931a4284d0eb7f3ef
Stanislav <stanichgame@gmail.com>
2021-04-09 20:11:57 +0300

    Stable version

--- backend/backend/static/js/base.js
+++ backend/backend/static/js/base.js
@@ -1,21 +1,4 @@
-document.getElementById('filter-button').onclick = function() {
-    let el = document.getElementById('filter');
-    el.style.width == '50px' ? el.style.width = '300px' : el.style.width = '50px';
-    el.style.height == '50px' ? el.style.height = '300px' : el.style.height = '50px';
-    el.style.background-color = 'pink';
+document.getElementsByClassName('filter-button')[0].onclick = function() {
+    let el = document.getElementsByClassName('filter-button')[0];
+    el.style.display == 'flex' ? el.style.display = 'none' : el.style.display = 'flex';
 }
-
-document.getElementById('slider').oninput = function getVar() {
-    let value = document.getElementById('filter-rating-scroll-value');
-    let slider = document.getElementById('slider');
-
-    value.innerHTML = slider.value;
-}
-
-let elm = document.querySelector('input');
-let container = elm.parentNode;
-let values = elm.getAttribute('data-values').split(' ');
-
-values.forEach(function (value, i, values) {
-    let rangePart = elm.cloneNode();
-})
---
+++ backend/backend/static_root/js/base.js
@@ -0,0 +1,5 @@
+function show() {
+    var el = document.getElementsByClassName('filter-button')[0];
+    console.log(el);
+    el.style.display = 'none';
+}
