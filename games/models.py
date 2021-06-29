09db7321dfe685bc161215d2ae4b5055e81191c8
Stanislau Semianenka <stanislau.semianenka@itechart-group.com>
2021-06-29 15:35:45 +0300

    Genre, Platform and Screenshot models added, other small changed

--- games/models.py
+++ games/models.py
@@ -3,17 +3,45 @@ from django.db import models
 from user.models import CustomUser
 
 
-class Game(models.Model):
+class AbstractModel(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(AbstractModel):
+    name = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}[{self.id}, {self.name}, {self.date_created}]'
+
+
+class Screenshot(AbstractModel):
+    url = models.URLField()
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}[{self.id}, {self.url}, {self.date_created}]'
+
+
+class Platform(AbstractModel):
+    name = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}[{self.id}, {self.name}, {self.date_created}]'
+
+
+class Game(AbstractModel):
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=128)
     short_description = models.CharField(max_length=128)
     logo = models.URLField()
     description = models.TextField(max_length=2048, null=True)
-    genres = models.CharField(default='', max_length=128, null=True)
-    platforms = models.CharField(default='', max_length=256, null=True)
+    genres = models.ManyToManyField(to=Genre)
+    platforms = models.ManyToManyField(to=Platform)
+    screenshots = models.ManyToManyField(to=Screenshot)
     date_release = models.CharField(default='', max_length=64, null=True)
-    screenshots = models.TextField(max_length=1024, null=True)
     ratings_users = models.CharField(default='', max_length=64, null=True)
     ratings_users_count = models.CharField(default='', max_length=64, null=True)
     ratings_critics = models.CharField(default='', max_length=64, null=True)
@@ -24,14 +52,17 @@ class Game(models.Model):
 
     def __repr__(self):
         return (
-            f'Game[{self.id}, {self.name}, {self.description[:32]}, {self.genres}, '
+            f'{self.__class__.__name__}[{self.id}, {self.name}, {self.description[:32]}, {self.genres}, '
             f'{self.date_release}, {self.date_created}'
         )
 
 
-class Musts(models.Model):
+class Musts(AbstractModel):
     game = models.ForeignKey(to=Game, on_delete=models.CASCADE)
     user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}[{self.id}, {self.game}, {self.user}, {self.date_created}]'
 
     class Meta:
         verbose_name = 'Musts'
