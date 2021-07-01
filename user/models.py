22d0edd14e7c9c34833363d9e25ff4b5e8f669f4
Stanislau Semianenka <stanislau.semianenka@itechart-group.com>
2021-07-01 12:12:54 +0300

    admin with '' default fields

--- user/models.py
+++ user/models.py
@@ -7,10 +7,10 @@ from .managers import UserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=16)
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=64, null=True)
-    last_name = models.CharField(max_length=64, null=True)
-    birth_date = models.DateField(auto_now=False, null=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=64, null=True, blank=True)
+    last_name = models.CharField(max_length=64, null=True, blank=True)
+    birth_date = models.DateField(auto_now=False, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
