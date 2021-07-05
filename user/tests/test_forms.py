8f988b1563194aa60a57b33b9c7f60c05539b7db
Stanislau Semianenka <stanislau.semianenka@itechart-group.com>
2021-07-05 10:07:56 +0300

    SignUpForm test clean()

--- user/tests/test_forms.py
+++ user/tests/test_forms.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
+from django.utils import timezone
 
-from user.forms import LoginForm
+from user.forms import SignUpForm
 
 
-class LoginFormTestCase(TestCase):
-    def test_form_fields(self):
-        form = LoginForm()
-        self.assertTrue(form.fields['username'].label in [None, 'username'])
+class SignUpFormTestCase(TestCase):
+    def test_form(self):
+        form_data = {
+            'username': 'custom_user',
+            'email': 'custom.mail@gmail.com',
+            'first_name': 'custom_name',
+            'last_name': 'custom_surname',
+            'birth_date': timezone.now().date(),
+            'password': 'Qwerty12345',
+            'confirm_password': 'Qwerty12345',
+        }
+        form = SignUpForm(form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['password'], form.cleaned_data['confirm_password'])
