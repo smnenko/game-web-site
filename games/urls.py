c6c5af8d9d5bb1272e3485a5b0679a1dcbb12064
Stanislav <36672532+smnenko@users.noreply.github.com>
2021-07-06 12:37:36 +0300

    Feature: game deleting (#6)
    * game deleting works (only admin group can do it)
    
    * deleting game via django DeleteView class
    
    Co-authored-by: Stanislau Semianenka <stanislau.semianenka@itechart-group.com>

--- games/urls.py
+++ games/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('page/<int:page>', views.IndexListView.as_view()),
     path('game/<int:game_id>', views.GameListView.as_view()),
     path('mymusts', views.MustsListView.as_view()),
-    path('must/<int:game_id>',views.MustView.as_view()),
-    path('search', views.SearchListView.as_view())
+    path('must/<int:game_id>', views.MustView.as_view()),
+    path('search', views.SearchListView.as_view()),
+    path('delete_game/<int:game_id>', views.GameDeleteView.as_view()),
 ]
