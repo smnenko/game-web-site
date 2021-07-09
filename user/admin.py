5fc2d5373136ecb95873c4ddab5b0fe9f0ca2150
Stanislav <36672532+smnenko@users.noreply.github.com>
2021-07-09 12:10:46 +0300

    .env integrated (#18)
    Co-authored-by: Stanislau Semianenka <stanislau.semianenka@itechart-group.com>

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import CustomUser
+from .models import Avatar
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'birth_date']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'birth_date', 'avatar']
+
+
+@admin.register(Avatar)
+class AvatarAdmin(admin.ModelAdmin):
+    list_display = ['avatar', 'pk']
